ccce5f6b21470351a6ede92b49a44ccdbaaa7772
Cédric Bontems <67456035+cbontems@users.noreply.github.com>
2024-02-01 17:09:08 +0100

    add log

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,4 +1,5 @@
 export default async (request, context) => {
+    context.log("Calling hello.js edge function.");
     return new Response('Hello, world from the edge!', {
         headers: {
             'content-type': 'text/html'
